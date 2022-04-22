4c5b4d716f908f3b0527d398f4159ea255427507
rafitinha <rafaoc7232@hotmail.com>
2022-04-22 00:31:45 +0000

    Deploying to gh-pages from @ rafitinha/next-spotify@d2960f57ef1d47461d61e7258ed41524273c03ba 🚀

---
+++ _next/static/3ZkN807UGi54oXMmTdXom/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/39f4d8aba4b66723.css","static/chunks/pages/index-1983aef8202d5099.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
